9da467210cb83dd396a227591eb9258a1e6decc1
hibana2077 <hibana2077@gmail.com>
2025-10-07 22:12:50 +0800

    feat: add initial job script for training video model with specified resources

---
+++ scripts/T/T002.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=16GB
+#PBS -l walltime=01:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+source /scratch/rp06/sl5952/AMBER/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/AMBER/.cache"
+
+cd ../..
+python3 -m src.run train --data-root "./datasets/ucf101_subset/temp_download/extracted/UCF101_subset" --output-dir "./runs/videomae-ucf" --epochs 50 --batch-size 32 --no_tqdm >> T002.log 2>&1
