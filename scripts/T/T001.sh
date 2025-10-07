7ec7f3dae340074ee8f920d6726757e32193b21e
hibana2077 <hibana2077@gmail.com>
2025-10-07 12:19:49 +0800

    fix: reorder commands in T001.sh for better execution flow

--- scripts/T/T001.sh
+++ scripts/T/T001.sh
@@ -10,8 +10,8 @@
 
 module load cuda/12.6.2
 source /scratch/rp06/sl5952/Mica/.venv/bin/activate
-
-cd ../..
 export HF_HOME="/scratch/rp06/sl5952/AMBER/.cache"
 
+cd ../..
+uv pip list > uv_installed.txt
 python3 -m src.run train --data-root "./datasets/ucf101_subset/temp_download/extracted/UCF101_subset" --output-dir "./runs/videomae-ucf" --epochs 4 --batch-size 4 >> T001.log 2>&1
