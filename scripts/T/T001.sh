a07c44992324c796a30c2063334e87e012d72f57
hibana2077 <hibana2077@gmail.com>
2025-10-07 12:32:37 +0800

    fix: update command to list installed packages in T001.sh

--- scripts/T/T001.sh
+++ scripts/T/T001.sh
@@ -13,5 +13,5 @@ source /scratch/rp06/sl5952/Mica/.venv/bin/activate
 export HF_HOME="/scratch/rp06/sl5952/AMBER/.cache"
 
 cd ../..
-python3 -m pip3 list >> uv_installed.txt
+ll .venv/lib/python3.11/site-packages/ >> uv_installed.txt
 python3 -m src.run train --data-root "./datasets/ucf101_subset/temp_download/extracted/UCF101_subset" --output-dir "./runs/videomae-ucf" --epochs 4 --batch-size 4 >> T001.log 2>&1
