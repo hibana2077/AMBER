30bdda52f8cd5a73e591dddc5e88fd2243106279
hibana2077 <hibana2077@gmail.com>
2025-10-08 09:30:23 +0800

    fix: update batch size in training script from 32 to 8

--- scripts/T/T002.sh
+++ scripts/T/T002.sh
@@ -13,4 +13,4 @@ source /scratch/rp06/sl5952/AMBER/.venv/bin/activate
 export HF_HOME="/scratch/rp06/sl5952/AMBER/.cache"
 
 cd ../..
-python3 -m src.run train --data-root "./datasets/ucf101_subset/temp_download/extracted/UCF101_subset" --output-dir "./runs/videomae-ucf" --epochs 50 --batch-size 32 --no_tqdm >> T002.log 2>&1
+python3 -m src.run train --data-root "./datasets/ucf101_subset/temp_download/extracted/UCF101_subset" --output-dir "./runs/videomae-ucf" --epochs 50 --batch-size 8 --no_tqdm >> T002.log 2>&1
